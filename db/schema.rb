89f19d58ab14ccba89d3cfc55e32ea7b7a992b23
kdh-r <kdh.r1008@gmail.com>
2020-09-06 20:43:48 +0900

    ユーザーテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_071501) do
     t.string "first_name", null: false
     t.string "last_name_kana", null: false
     t.string "first_name_kana", null: false
-    t.date "birthday_id", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

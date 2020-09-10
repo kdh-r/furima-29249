a72d07344090a762c6cff8b37c20a3b0ada1c7c5
kdh-r <kdh.r1008@gmail.com>
2020-09-10 20:55:53 +0900

    アクティブハッシュ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
+  
   def index
+    @items = Item.order("created_at DESC")
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+
+
+  private
+  def item_params
+     params.require(:item).permit(:name, :text, :category_id, :status_id, :postage_type_id, :ship_from_id, :delivery_time_id, :price )
+  end
+
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :postage_type
+  belongs_to_active_hash :ship_from
+  belongs_to_active_hash :delivery_time
+
+
+
+  belongs_to :user
+  has_one :buy
+
+
+  validates :name, :text, :category_id, :status_id, :postage_type_id, :ship_from_id, :delivery_time_id, :price, presence: true
+  validates :category_id, :status_id, :postage_type_id, :ship_from_id, :delivery_time_id, numericality: { other_than: 1 }
+  # validates :price, format:{ with:
+end

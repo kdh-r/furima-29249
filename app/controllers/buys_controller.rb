494f152c644fec543aec216742ca11f19320c4a2
kdh-r <kdh.r1008@gmail.com>
2020-09-17 15:31:03 +0900

    payjp, formオブジェクト関連

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -5,8 +5,33 @@ class BuysController < ApplicationController
   end
 
   def create
-    
+    @item = Item.find(params[:item_id])
+    @buy = BuyAddress.new(buy_params)
+    if @buy.valid?            
+      pay_item 
+      @buy.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+
   end
 
-  
+
+ private
+
+    def buy_params
+      params.permit( :token, :postal_code, :prefecture_id, :city,
+                     :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+    end
+    
+    #決済処理
+    def pay_item
+      Payjp.api_key = "sk_test_2f93eb7abef01424d342b232"
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buy_params[:token],
+        currency:'jpy'
+      )
+    end
 end
--- app/models/user_address.rb
+++ app/models/user_address.rb
@@ -1,8 +1,4 @@
 class UserAddress < ApplicationRecord
-
   belongs_to :buy
 
-  validates :postal_code, :prefecture_id, :city, :address, :phone_number, presence: true
-  validates :postal_code, format:{ with: /\A\d{3}[-]\d{4}\z/}
-  validates :phone_number, format{ :with /\A\d{10}\z|\A\d{11}\z/}
 end

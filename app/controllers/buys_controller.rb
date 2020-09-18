b5daa6b501441a739d3184daee0aca4db92d4abb
kdh-r <kdh.r1008@gmail.com>
2020-09-18 19:55:41 +0900

    修正1

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,14 +1,15 @@
 class BuysController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_buy, only: [:index, :create]
+
+
 
   def index
-    @item = Item.find(params[:item_id])
     @buy = BuyAddress.new
     return redirect_to root_path if @item.user_id == current_user.id || @item.buy # 後置ifを使用
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @buy = BuyAddress.new(buy_params)
     if @buy.valid?
       pay_item
@@ -26,9 +27,15 @@ class BuysController < ApplicationController
                   :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 
+
+  def set_buy
+    @item = Item.find(params[:item_id])
+  end
+
+
   # 決済処理
   def pay_item
-    Payjp.api_key = 'sk_test_2f93eb7abef01424d342b232'
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: buy_params[:token],
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_a7bcf258caab20fdf643e4a3");// PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);// PAY.JPテスト公開鍵
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
